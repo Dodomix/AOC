54c09381a01da060fe6bb32bd1432f259aa1c1b0
dzigrovic <dominik.zigrovic@altima.hr>
2020-11-30 11:18:26 +0100

    Prepare 2020 environment

---
+++ 2020/src/main/kotlin/hr/dodomix/advent/solutions/Day1.kt
@@ -0,0 +1,11 @@
+package hr.dodomix.advent.solutions
+
+import hr.dodomix.advent.util.Util
+
+class Day1 {
+    fun dayDirectory() = "day1"
+
+    fun part1(input: List<String>): Int = 0
+
+    fun part2(input: List<String>): Int = 0
+}
---
+++ 2020/src/main/kotlin/hr/dodomix/advent/solutions/Main.kt
@@ -0,0 +1,10 @@
+package hr.dodomix.advent.solutions
+
+import hr.dodomix.advent.util.Util
+
+fun main() {
+    val day = Day1()
+    val dayDirectory = day.dayDirectory()
+    println("Part 1 result: " + day.part1(Util.readFileLines("$dayDirectory/input1")))
+    println("Part 2 result: " + day.part2(Util.readFileLines("$dayDirectory/input1")))
+}
