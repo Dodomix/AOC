f9406c916e746019961f5d09d05f11ba829a258f
dzigrovic <dominik.zigrovic@altima.hr>
2020-12-01 08:50:25 +0100

    Add day 1 solutions

--- 2020/src/main/kotlin/hr/dodomix/advent/solutions/Day1.kt
+++ 2020/src/main/kotlin/hr/dodomix/advent/solutions/Day1.kt
@@ -1,11 +1,29 @@
 package hr.dodomix.advent.solutions
 
-import hr.dodomix.advent.util.Util
-
 class Day1 {
     fun dayDirectory() = "day1"
 
-    fun part1(input: List<String>): Int = 0
+    fun part1(input: List<String>): Int? {
+        val expenses = input.map { it.toInt() }
+        expenses.forEach { expense1 ->
+            expenses.find { expense2 -> expense1 + expense2 == 2020 }
+                ?.also { expense2 ->
+                    return expense1 * expense2
+                }
+        }
+        return null
+    }
 
-    fun part2(input: List<String>): Int = 0
+    fun part2(input: List<String>): Int? {
+        val expenses = input.map { it.toInt() }
+        expenses.forEach { expense1 ->
+            expenses.forEach { expense2 ->
+                expenses.find { expense3 -> expense1 + expense2 + expense3 == 2020 }
+                    ?.also { expense3 ->
+                        return expense1 * expense2 * expense3
+                    }
+            }
+        }
+        return null
+    }
 }
