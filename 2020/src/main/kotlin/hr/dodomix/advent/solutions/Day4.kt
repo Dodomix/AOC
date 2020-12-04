eab817cd7d14858690aea1d64a37f9a6cf9b7ef3
Dodomix <domi.zigrovic@gmail.com>
2020-12-04 17:32:49 +0100

    Improve day 4 code

--- 2020/src/main/kotlin/hr/dodomix/advent/solutions/Day4.kt
+++ 2020/src/main/kotlin/hr/dodomix/advent/solutions/Day4.kt
@@ -5,22 +5,13 @@ class Day4 {
 
     fun part1(input: List<String>): Int {
         val requiredFields = listOf("byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:")
-        var passport = ""
-        var count = 0
-        input.forEach {
-            if (it.isEmpty()) {
-                if (requiredFields.all { field -> passport.contains(field) }) {
-                    count++
-                    println(1)
-                } else {
-                    println(0)
-                }
-                passport = ""
+        return input.fold(Pair("", 0)) { (passport, count), line ->
+            if (line.isEmpty()) {
+                Pair("", count + if (requiredFields.all { field -> passport.contains(field) }) 1 else 0)
             } else {
-                passport += it
+                Pair(passport + line, count)
             }
-        }
-        return count
+        }.second
     }
 
     fun part2(input: List<String>): Int {
@@ -29,35 +20,30 @@ class Day4 {
             Pair("iyr", { value: String -> value.toInt() in 2010..2020 }),
             Pair("eyr", { value: String -> value.toInt() in 2020..2030 }),
             Pair("hgt", { value: String ->
-                val numericalValue = value.dropLast(2)
-                when {
-                    value.endsWith("cm") -> numericalValue.toInt() in 150..193
-                    value.endsWith("in") -> numericalValue.toInt() in 59..76
-                    else -> false
+                value.dropLast(2).let {
+                    when {
+                        value.endsWith("cm") -> it.toInt() in 150..193
+                        value.endsWith("in") -> it.toInt() in 59..76
+                        else -> false
+                    }
                 }
             }),
             Pair("hcl", { value: String -> value.matches("#[0-9a-f]{6}".toRegex()) }),
             Pair("ecl", { value: String -> value in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth") }),
             Pair("pid", { value: String -> value.matches("[0-9]{9}".toRegex()) })
         )
-        val passport = mutableMapOf<String, String>()
-        var count = 0
-        input.forEach {
-            if (it.isEmpty()) {
-                if (requiredFieldValidators.all { entry ->
-                        passport.containsKey(entry.key) &&
-                            entry.value(passport.getValue(entry.key))
-                    }) {
-                    count++
-                }
-                passport.clear()
+        return input.fold(Pair(mapOf<String, String>(), 0)) { (passport, count), line ->
+            if (line.isEmpty()) {
+                Pair(mapOf(), count + if (requiredFieldValidators.all { (key, validator) ->
+                        passport.containsKey(key) && validator(passport.getValue(key))
+                    }) 1 else 0)
             } else {
-                it.split(" ").forEach { fieldValue ->
-                    val fieldValueSplit = fieldValue.split(":")
-                    passport += Pair(fieldValueSplit[0], fieldValueSplit[1])
-                }
+                Pair(passport + line.split(" ").map { fieldValue ->
+                    fieldValue.split(":").let { (key, value) ->
+                        Pair(key, value)
+                    }
+                }, count)
             }
-        }
-        return count
+        }.second
     }
 }
