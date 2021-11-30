27a90abf65e558d6895618592e9b4c944cd55f2b
dzigrovic <dominik.zigrovic@altima.hr>
2021-11-30 07:14:37 +0100

    Prepare for 2021

---
+++ 2021/src/main/kotlin/hr/dodomix/advent/solutions/Day1.kt
@@ -0,0 +1,13 @@
+package hr.dodomix.advent.solutions
+
+class Day1 {
+    fun dayDirectory() = "day1"
+
+    fun part1(input: List<String>): Int? {
+        return null
+    }
+
+    fun part2(input: List<String>): Int? {
+        return null
+    }
+}
