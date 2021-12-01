5eaa0ee456be68abfc3b79cc2f8b8870258408b0
dzigrovic <dominik.zigrovic@altima.hr>
2021-12-01 06:42:33 +0100

    Add day 1 solutions

--- 2021/src/main/kotlin/hr/dodomix/advent/solutions/Day1.kt
+++ 2021/src/main/kotlin/hr/dodomix/advent/solutions/Day1.kt
@@ -3,11 +3,16 @@ package hr.dodomix.advent.solutions
 class Day1 {
     fun dayDirectory() = "day1"
 
-    fun part1(input: List<String>): Int? {
-        return null
+    fun part1(input: List<String>): Int {
+        return countIncreases(input.map { it.toInt() })
     }
 
-    fun part2(input: List<String>): Int? {
-        return null
+    fun part2(input: List<String>): Int {
+        val inputAsInteger = input.map { it.toInt() }
+        return countIncreases(inputAsInteger.zip(inputAsInteger.drop(1)).zip(inputAsInteger.drop(2)).map {
+            it.first.first + it.first.second + it.second
+        })
     }
+
+    private fun countIncreases(input: List<Int>): Int = input.zipWithNext().count { it.first < it.second }
 }
