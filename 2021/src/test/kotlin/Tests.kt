1dfa0895554c7890dbd7eae23ea7727502717283
dzigrovic <dominik.zigrovic@altima.hr>
2021-12-03 07:36:11 +0100

    Add day 3 tests

--- 2021/src/test/kotlin/Tests.kt
+++ 2021/src/test/kotlin/Tests.kt
@@ -1,5 +1,6 @@
 import hr.dodomix.advent.solutions.Day1
 import hr.dodomix.advent.solutions.Day2
+import hr.dodomix.advent.solutions.Day3
 import hr.dodomix.advent.util.Util
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -34,6 +35,22 @@ class Tests {
         fun part2() {
             assertThat(day.part2(Util.readFileLines("${day.dayDirectory()}/test-input1")))
                 .isEqualTo(900)
+        }
+    }
+
+    internal class Day3Test {
+        private val day = Day3()
+
+        @Test
+        fun part1() {
+            assertThat(day.part1(Util.readFileLines("${day.dayDirectory()}/test-input1")))
+                .isEqualTo(198)
+        }
+
+        @Test
+        fun part2() {
+            assertThat(day.part2(Util.readFileLines("${day.dayDirectory()}/test-input1")))
+                .isEqualTo(230)
         }
     }
 }
