a72fbd9c43969094d039e8d66e19a0df2099c0a0
dzigrovic <dominik.zigrovic@altima.hr>
2021-12-04 08:42:36 +0100

    Add day 4 tests

--- 2021/src/test/kotlin/Tests.kt
+++ 2021/src/test/kotlin/Tests.kt
@@ -1,6 +1,7 @@
 import hr.dodomix.advent.solutions.Day1
 import hr.dodomix.advent.solutions.Day2
 import hr.dodomix.advent.solutions.Day3
+import hr.dodomix.advent.solutions.Day4
 import hr.dodomix.advent.util.Util
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -51,6 +52,22 @@ class Tests {
         fun part2() {
             assertThat(day.part2(Util.readFileLines("${day.dayDirectory()}/test-input1")))
                 .isEqualTo(230)
+        }
+    }
+
+    internal class Day4Test {
+        private val day = Day4()
+
+        @Test
+        fun part1() {
+            assertThat(day.part1(Util.readFileLines("${day.dayDirectory()}/test-input1")))
+                .isEqualTo(4512)
+        }
+
+        @Test
+        fun part2() {
+            assertThat(day.part2(Util.readFileLines("${day.dayDirectory()}/test-input1")))
+                .isEqualTo(1924)
         }
     }
 }
