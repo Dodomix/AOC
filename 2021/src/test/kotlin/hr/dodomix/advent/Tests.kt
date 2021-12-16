dad05d036110ae3fc1cd7677c596cddd9a5311d9
dzigrovic <dominik.zigrovic@altima.hr>
2021-12-16 09:41:49 +0100

    Add day 16 tests

--- 2021/src/test/kotlin/hr/dodomix/advent/Tests.kt
+++ 2021/src/test/kotlin/hr/dodomix/advent/Tests.kt
@@ -7,6 +7,7 @@ import hr.dodomix.advent.solutions.Day12
 import hr.dodomix.advent.solutions.Day13
 import hr.dodomix.advent.solutions.Day14
 import hr.dodomix.advent.solutions.Day15
+import hr.dodomix.advent.solutions.Day16
 import hr.dodomix.advent.solutions.Day2
 import hr.dodomix.advent.solutions.Day3
 import hr.dodomix.advent.solutions.Day4
@@ -19,6 +20,8 @@ import hr.dodomix.advent.util.Util
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 
 class Tests {
@@ -268,6 +271,39 @@ class Tests {
         fun part2() {
             assertThat(day.part2(Util.readFileLines("${day.dayDirectory()}/test-input1")))
                 .isEqualTo(315)
+        }
+    }
+
+    @Nested
+    inner class Day16Test {
+        private val day = Day16()
+
+        @ParameterizedTest
+        @CsvSource(
+            "8A004A801A8002F478,16",
+            "620080001611562C8802118E34,12",
+            "C0015000016115A2E0802F182340,23",
+            "A0016C880162017C3686B18A3D4780,31"
+        )
+        fun part1(input: String, result: Int) {
+            assertThat(day.part1(listOf(input)))
+                .isEqualTo(result)
+        }
+
+        @ParameterizedTest
+        @CsvSource(
+            "C200B40A82,3",
+            "04005AC33890,54",
+            "880086C3E88112,7",
+            "CE00C43D881120,9",
+            "D8005AC2A8F0,1",
+            "F600BC2D8F,0",
+            "9C005AC2F8F0,0",
+            "9C0141080250320F1802104A08,1"
+        )
+        fun part2(input: String, result: Int) {
+            assertThat(day.part2(listOf(input)))
+                .isEqualTo(result)
         }
     }
 }
