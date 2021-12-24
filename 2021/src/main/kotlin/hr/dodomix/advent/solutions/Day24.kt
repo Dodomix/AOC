fbf38d05a35509514b33c1c9e3da11beacd6d768
dzigrovic <dominik.zigrovic@altima.hr>
2021-12-24 10:33:18 +0100

    Add day 24 solutions

---
+++ 2021/src/main/kotlin/hr/dodomix/advent/solutions/Day24.kt
@@ -0,0 +1,13 @@
+package hr.dodomix.advent.solutions
+
+class Day24 {
+    fun dayDirectory() = "day24"
+
+    fun part1(input: List<String>): String {
+        return "Done manually"
+    }
+
+    fun part2(input: List<String>): String {
+        return "Done manually"
+    }
+}
