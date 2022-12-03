df22118bdc190d4e0aa537069a45c1977747f6a7
Dominik Žigrović <domi.zigrovic@gmail.com>
2022-12-03 09:17:19 +0100

    Add day 3 tests

--- 2022/src/test/kotlin/hr/dodomix/advent/Tests.kt
+++ 2022/src/test/kotlin/hr/dodomix/advent/Tests.kt
@@ -2,6 +2,7 @@ package hr.dodomix.advent
 
 import hr.dodomix.advent.solutions.Day1
 import hr.dodomix.advent.solutions.Day2
+import hr.dodomix.advent.solutions.Day3
 import hr.dodomix.advent.util.Util
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
@@ -40,6 +41,23 @@ class Tests {
         fun part2() {
             assertThat(day.part2(Util.readFileLines("${day.dayDirectory()}/test-input1")))
                 .isEqualTo(12)
+        }
+    }
+
+    @Nested
+    inner class Day3Test {
+        private val day = Day3()
+
+        @Test
+        fun part1() {
+            assertThat(day.part1(Util.readFileLines("${day.dayDirectory()}/test-input1")))
+                .isEqualTo(157)
+        }
+
+        @Test
+        fun part2() {
+            assertThat(day.part2(Util.readFileLines("${day.dayDirectory()}/test-input1")))
+                .isEqualTo(70)
         }
     }
 }
